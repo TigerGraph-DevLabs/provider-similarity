1855d50c71ffea2f339c2926c9e30d687552aa5b
Parker Erickson <parker.erickson@tigergraph.com>
2021-10-02 19:16:37 -0500

    add set num devices

--- fastrp_similarity_hw_accel.py
+++ fastrp_similarity_hw_accel.py
@@ -4,12 +4,14 @@ import time
 
 conn = tg.TigerGraphConnection(host="http://1.1.1.13", graphname="ProviderSimilarity")
 
-precached = True
+precached = False
 
 if not precached:
+    print("===== Caching Embeddings =====")
+    setNumDevices = conn.runInstalledQuery("cosinesim_set_num_devices", params={"num_devicese":7})
     retCode = conn.runInstalledQuery("tg_load_graph_cosinesim_ss_fpga_core", params={"vert_types":"Individual"}, timeout=1_024_000)
     print(retCode)
-
+    print("===== Embeddings Cached ======\n")
 #top10 = conn.runInstalledQuery("tg_cosinesim_ss_fpga_core", params={"source": "1457353518", "source.type": "Individual", "topK": 10})[0]["@@results"]
 providers = ["1790766392", "1669475711", "1154324184", "1275536443", "1225031859",
              "1114920642", "1366444697", "1417959669", "1386318749", "1659576965"]
