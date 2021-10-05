9dc7abc08791f240373e3c64880d3e71255a4ec5
TigerGraph <parker.erickson@tigergraph.com>
2021-10-05 04:41:14 -0500

    Final HW push

--- fastrp_similarity_hw_accel.py
+++ fastrp_similarity_hw_accel.py
@@ -4,17 +4,18 @@ import time
 
 conn = tg.TigerGraphConnection(host="http://1.1.1.13", graphname="ProviderSimilarity")
 
-precached = False
+precached = True
 
 if not precached:
     print("===== Caching Embeddings =====")
-    setNumDevices = conn.runInstalledQuery("cosinesim_set_num_devices", params={"num_devicese":7})
+    setNumDevices = conn.runInstalledQuery("cosinesim_set_num_devices", params={"num_devices":"7"})
     retCode = conn.runInstalledQuery("tg_load_graph_cosinesim_ss_fpga_core", params={"vert_types":"Individual"}, timeout=1_024_000)
     print(retCode)
     print("===== Embeddings Cached ======\n")
 #top10 = conn.runInstalledQuery("tg_cosinesim_ss_fpga_core", params={"source": "1457353518", "source.type": "Individual", "topK": 10})[0]["@@results"]
-providers = ["1790766392", "1669475711", "1154324184", "1275536443", "1225031859",
-             "1114920642", "1366444697", "1417959669", "1386318749", "1659576965"]
+providers = ["1790766392", "1184626384", "1164424461", "1235131483", "1841292943",
+             "1578565859", "1871597104", "1629072996", "1083696371", "1790766392"]
+
 times = []
 for provider in providers:
     t1 = time.time()
